d637d834316dccaad8caf3cfab88a08798e05e43
Piotr Jarema <jaremap@bpol0848.emea.local>
2021-12-21 13:19:24 +0100

    React components

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Aside from './components/Aside/Aside';
+import Sidebar from './components/Sidebar/Sidebar';
+import "./app.css";
+import background from "./assets/img/pika.png";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ backgroundImage: `url(${background})` }}>
+      <section className="wrapper">
+        <Sidebar />
+        <Aside />
+      </section>
     </div>
   );
 }
---
+++ client/src/components/Aside/Aside.js
@@ -0,0 +1,11 @@
+import {Component} from 'react';
+
+class Aside extends Component {
+    render() {
+        return(
+          <h1>Aside</h1>
+        );
+    }
+}
+
+export default Aside;
